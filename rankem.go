019738ba9ba51546fa7d86ec544208ea70529743
Carl Johnson <me@carlmjohnson.net>
2018-04-26 21:01:52 -0400

    Switch to Binary Insertion Sort

--- rankem.go
+++ rankem.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 
 	"github.com/manifoldco/promptui"
 )
@@ -12,17 +11,17 @@ import (
 func main() {
 	fmt.Print("Items to rank:\n\n")
 
-	r := newByRank()
+	r := []string{}
 	for {
 		item := readline()
 		if item == "" {
 			break
 		}
-		r.Push(item)
+		r = append(r, item)
 	}
 	check(scanner.Err())
-	sort.Sort(r)
-	for i, s := range r.arr {
+	BinaryInsertionSort(r, LessPrompt)
+	for i, s := range r {
 		fmt.Printf("%d. %s\n", i+1, s)
 	}
 }
@@ -43,41 +42,38 @@ func check(err error) {
 	}
 }
 
-type byRank struct {
-	arr []string
-	s   map[[2]string]bool
-}
-
-func newByRank() *byRank {
-	return &byRank{s: make(map[[2]string]bool)}
-}
-
-func (r *byRank) Len() int {
-	return len(r.arr)
-}
-
-func (r *byRank) Swap(i, j int) {
-	r.arr[i], r.arr[j] = r.arr[j], r.arr[i]
-}
-
-func (r *byRank) Less(i, j int) bool {
-	pair := [2]string{r.arr[i], r.arr[j]}
-	if b, ok := r.s[pair]; ok {
-		return b
-	}
-
-	oppPair := [2]string{r.arr[j], r.arr[i]}
-	if b, ok := r.s[oppPair]; ok {
-		return !b
-	}
-
-	p := promptui.Select{Label: "Which ranks higher", Items: pair[:]}
+func LessPrompt(a, b string) bool {
+	p := promptui.Select{Label: "Which ranks higher", Items: []string{a, b}}
 	choice, _, err := p.Run()
 	check(err)
-	r.s[pair] = choice == 0
 	return choice == 0
 }
 
-func (r *byRank) Push(x string) {
-	r.arr = append(r.arr, x)
+func BinaryInsertionSort(a []string, less func(a, b string) bool) {
+	var hi, lo int
+
+	for i := 1; i < len(a); i++ {
+		lo = 0
+		hi = i
+		m := i / 2
+
+		for {
+			if less(a[i], a[m]) {
+				hi = m
+			} else {
+				lo = m + 1
+			}
+
+			m = lo + ((hi - lo) / 2)
+			if lo >= hi {
+				break
+			}
+		}
+
+		if m < i {
+			tmp := a[i]
+			copy(a[m+1:], a[m:i])
+			a[m] = tmp
+		}
+	}
 }
